0e6e58db592b3be69b711ed381b4ba24e9480bda
polcortes <122600020+polcortes@users.noreply.github.com>
2023-01-13 20:07:54 +0100

    Add files via upload

---
+++ pruebas_play_game.py
@@ -0,0 +1,158 @@
+baraja_española = {
+                # ---------------------------- OROS / COINS
+                "G01": {"literal": "Ace of Coins", "value": 1, "priority": 4, "realValue": 1},
+                "G02": {"literal": "Two of Coins", "value": 2, "priority": 4, "realValue": 2},
+                "G03": {"literal": "Three of Coins", "value": 3, "priority": 4, "realValue": 3},
+                "G04": {"literal": "Four of Coins", "value": 4, "priority": 4, "realValue": 4},
+                "G05": {"literal": "Five of Coins", "value": 5, "priority": 4, "realValue": 5},
+                "G06": {"literal": "Six of Coins", "value": 6, "priority": 4, "realValue": 6},
+                "G07": {"literal": "Seven of Coins", "value": 7, "priority": 4, "realValue": 7},
+                "G08": {"literal": "Eight of Coins", "value": 8, "priority": 4, "realValue": 0.5},
+                "G09": {"literal": "Nine of Coins", "value": 9, "priority": 4, "realValue": 0.5},
+                "G10": {"literal": "Jack of Coins", "value": 10, "priority": 4, "realValue": 0.5},
+                "G11": {"literal": "Horse of Coins", "value": 11, "priority": 4, "realValue": 0.5},
+                "G12": {"literal": "King of Coins", "value": 12, "priority": 4, "realValue": 0.5},
+                # ---------------------------- COPAS / CUPS
+                "C01": {"literal": "Ace of Cups", "value": 1, "priority": 3, "realValue": 1},
+                "C02": {"literal": "Two of Cups", "value": 2, "priority": 3, "realValue": 2},
+                "C03": {"literal": "Three of Cups", "value": 3, "priority": 3, "realValue": 3},
+                "C04": {"literal": "Four of Cups", "value": 4, "priority": 3, "realValue": 4},
+                "C05": {"literal": "Five of Cups", "value": 5, "priority": 3, "realValue": 5},
+                "C06": {"literal": "Six of Cups", "value": 6, "priority": 3, "realValue": 6},
+                "C07": {"literal": "Seven of Cups", "value": 7, "priority": 3, "realValue": 7},
+                "C08": {"literal": "Eight of Cups", "value": 8, "priority": 3, "realValue": 0.5},
+                "C09": {"literal": "Nine of Cups", "value": 9, "priority": 3, "realValue": 0.5},
+                "C10": {"literal": "Jack of Cups", "value": 10, "priority": 3, "realValue": 0.5},
+                "C11": {"literal": "Horse of Cups", "value": 11, "priority": 3, "realValue": 0.5},
+                "C12": {"literal": "King of Cups", "value": 12, "priority": 3, "realValue": 0.5},
+                # ---------------------------- SWORDS / ESPADAS
+                "S01": {"literal": "Ace of Swords", "value": 1, "priority": 2, "realValue": 1},
+                "S02": {"literal": "Two of Swords", "value": 2, "priority": 2, "realValue": 2},
+                "S03": {"literal": "Three of Swords", "value": 3, "priority": 2, "realValue": 3},
+                "S04": {"literal": "Four of Swords", "value": 4, "priority": 2, "realValue": 4},
+                "S05": {"literal": "Five of Swords", "value": 5, "priority": 2, "realValue": 5},
+                "S06": {"literal": "Six of Swords", "value": 6, "priority": 2, "realValue": 6},
+                "S07": {"literal": "Seven of Swords", "value": 7, "priority": 2, "realValue": 7},
+                "S08": {"literal": "Eight of Swords", "value": 8, "priority": 2, "realValue": 0.5},
+                "S09": {"literal": "Nine of Swords", "value": 9, "priority": 2, "realValue": 0.5},
+                "S10": {"literal": "Jack of Swords", "value": 10, "priority": 2, "realValue": 0.5},
+                "S11": {"literal": "Horse of Swords", "value": 11, "priority": 2, "realValue": 0.5},
+                "S12": {"literal": "King of Swords", "value": 12, "priority": 2, "realValue": 0.5},
+                # ---------------------------- CLUBS / BASTOS
+                "B01": {"literal": "Ace of Clubs", "value": 1, "priority": 1, "realValue": 1},
+                "B02": {"literal": "Two of Clubs", "value": 2, "priority": 1, "realValue": 2},
+                "B03": {"literal": "Three of Clubs", "value": 3, "priority": 1, "realValue": 3},
+                "B04": {"literal": "Four of Clubs", "value": 4, "priority": 1, "realValue": 4},
+                "B05": {"literal": "Five of Clubs", "value": 5, "priority": 1, "realValue": 5},
+                "B06": {"literal": "Six of Clubs", "value": 6, "priority": 1, "realValue": 6},
+                "B07": {"literal": "Seven of Clubs", "value": 7, "priority": 1, "realValue": 7},
+                "B08": {"literal": "Eight of Clubs", "value": 8, "priority": 1, "realValue": 0.5},
+                "B09": {"literal": "Nine of Clubs", "value": 9, "priority": 1, "realValue": 0.5},
+                "B10": {"literal": "Jack of Clubs", "value": 10, "priority": 1, "realValue": 0.5},
+                "B11": {"literal": "Horse of Clubs", "value": 11, "priority": 1, "realValue": 0.5},
+                "B12": {"literal": "King of Clubs", "value": 12, "priority": 1, "realValue": 0.5}
+}
+
+
+baraja_poker = {# ----------------------- Tréboles / Clubs
+                "C01":{"literal":"One of Clubs", "value": 1, "priority": 4, "realValue": 1},
+                "C02":{"literal":"Two of Clubs", "value":2, "priority":4, "realValue":2},
+                "C03":{"literal":"Three of Clubs", "value":3, "priority":4, "realValue":3},
+                "C04":{"literal":"Four of Clubs", "value":4, "priority":4, "realValue":4},
+                "C05":{"literal":"Five of Clubs", "value":5, "priority":4, "realValue":5},
+                "C06":{"literal":"Six of Clubs", "value":6, "priority":4, "realValue":6},
+                "C07":{"literal":"Seven of Clubs", "value":7, "priority":4, "realValue":7},
+                "C08":{"literal":"Eight of Clubs", "value":8, "priority":4, "realValue":0.5},
+                "C09":{"literal":"Nine of Clubs", "value":9, "priority":4, "realValue":0.5},
+                "C10":{"literal":"Ten of Clubs", "value":10, "priority":4, "realValue":0.5},
+                "C11":{"literal":"Jack of Clubs", "value":11, "priority":4, "realValue":0.5},
+                "C12":{"literal":"Queen of Clubs", "value":12, "priority":4, "realValue":0.5},
+                "C13":{"literal":"King of Clubs", "value":13, "priority":4, "realValue":0.5},
+                "C14":{"literal":"Ace of Clubs", "value":14, "priority":4, "realValue":0.5},
+                # ----------------------- Picas / Spades
+                "S01":{"literal":"One of Spades", "value": 1, "priority": 3, "realValue": 1},
+                "S02":{"literal":"Two of Spades", "value":2, "priority":3, "realValue":2},
+                "S03":{"literal":"Three of Spades", "value":3, "priority":3, "realValue":3},
+                "S04":{"literal":"Four of Spades", "value":4, "priority":3, "realValue":4},
+                "S05":{"literal":"Five of Spades", "value":5, "priority":3, "realValue":5},
+                "S06":{"literal":"Six of Spades", "value":6, "priority":3, "realValue":6},
+                "S07":{"literal":"Seven of Spades", "value":7, "priority":3, "realValue":7},
+                "S08":{"literal":"Eight of Spades", "value":8, "priority":3, "realValue":0.5},
+                "S09":{"literal":"Nine of Spades", "value":9, "priority":3, "realValue":0.5},
+                "S10":{"literal":"Ten of Spades", "value":10, "priority":3, "realValue":0.5},
+                "S11":{"literal":"Jack of Spades", "value":11, "priority":3, "realValue":0.5},
+                "S12":{"literal":"Queen of Spades", "value":12, "priority":3, "realValue":0.5},
+                "S13":{"literal":"King of Spades", "value":13, "priority":3, "realValue":0.5},
+                "S14":{"literal":"Ace of Spades", "value":14, "priority":3, "realValue":0.5},
+                # ----------------------- Corazones / Hearts
+                "H01":{"literal":"One of Hearts", "value": 1, "priority": 2, "realValue": 1},
+                "H02":{"literal":"Two of Hearts", "value":2, "priority":2, "realValue":2},
+                "H03":{"literal":"Three of Hearts", "value":3, "priority":2, "realValue":3},
+                "H04":{"literal":"Four of Hearts", "value":4, "priority":2, "realValue":4},
+                "H05":{"literal":"Five of Hearts", "value":5, "priority":2, "realValue":5},
+                "H06":{"literal":"Six of Hearts", "value":6, "priority":2, "realValue":6},
+                "H07":{"literal":"Seven of Hearts", "value":7, "priority":2, "realValue":7},
+                "H08":{"literal":"Eight of Hearts", "value":8, "priority":2, "realValue":0.5},
+                "H09":{"literal":"Nine of Hearts", "value":9, "priority":2, "realValue":0.5},
+                "H10":{"literal":"Ten of Hearts", "value":10, "priority":2, "realValue":0.5},
+                "H11":{"literal":"Jack of Hearts", "value":11, "priority":2, "realValue":0.5},
+                "H12":{"literal":"Queen of Hearts", "value":12, "priority":2, "realValue":0.5},
+                "H13":{"literal":"King of Hearts", "value":13, "priority":2, "realValue":0.5},
+                "H14":{"literal":"Ace of Hearts", "value":14, "priority":2, "realValue":0.5},
+                # ----------------------- Diamantes / Diamonds
+                "D01":{"literal":"One of Diamonds", "value": 1, "priority": 1, "realValue": 1},
+                "D02":{"literal":"Two of Diamonds", "value":2, "priority":1, "realValue":2},
+                "D03":{"literal":"Three of Diamonds", "value":3, "priority":1, "realValue":3},
+                "D04":{"literal":"Four of Diamonds", "value":4, "priority":1, "realValue":4},
+                "D05":{"literal":"Five of Diamonds", "value":5, "priority":1, "realValue":5},
+                "D06":{"literal":"Six of Diamonds", "value":6, "priority":1, "realValue":6},
+                "D07":{"literal":"Seven of Diamonds", "value":7, "priority":1, "realValue":7},
+                "D08":{"literal":"Eight of Diamonds", "value":8, "priority":1, "realValue":0.5},
+                "D09":{"literal":"Nine of Diamonds", "value":9, "priority":1, "realValue":0.5},
+                "D10":{"literal":"Ten of Diamonds", "value":10, "priority":1, "realValue":0.5},
+                "D11":{"literal":"Jack of Diamonds", "value":11, "priority":1, "realValue":0.5},
+                "D12":{"literal":"Queen of Diamonds", "value":12, "priority":1, "realValue":0.5},
+                "D13":{"literal":"King of Diamonds", "value":13, "priority":1, "realValue":0.5},
+                "D14":{"literal":"Ace of Diamonds", "value":14, "priority":1, "realValue":0.5}}
+
+baraja = baraja_española
+
+players = {"11115555A":{"name":"Mario", "human":True, "bank":False, "initialCard":"", "priority":0, "type":40, "bet":4, 
+                        "points":0, "cards":[], "roundPoints":0},
+           "22225555A":{"name":"Pedro", "human":True, "bank":False, "initialCard":"", "priority":0, "type":40, "bet":4, 
+                        "points":0, "cards":[], "roundPoints":0},
+           "40240157R":{"name":"bot", "human":False, "bank":False, "initialCard":"", "priority":0, "type":40, "bet":4, 
+                        "points":0, "cards":[], "roundPoints":0}}
+
+game = ["11115555A", "22225555A", "40240157R"]   # IDs de los jugadores/bots
+
+mazo = ["D14, D07"]   # IDs de todas las cartas que se usen en la partida
+
+context_game = {1:game,
+                2:mazo}
+            # ID de partida
+cardgame = {}
+          # "cardgame_id":0, "players":0, "start_hour":0, "rounds":0, "final_hour":0
+
+from datetime import datetime
+
+hora_actual = datetime.now()
+hora_actual = hora_actual.strftime("%H:%M:%S")
+print(f"Ahora mismo son las {hora_actual}")
+input('TOCA PARA EL SIGUIENTE')
+
+opc = None
+
+print("1) Ver jugadores en partida\n2) Ver cartas descartadas\n3) Jugar (de verdad)")
+opc = input("Opción\n> ")
+opc = int(opc)
+if opc == 3:
+    print('Estas jugando :v')
+    hora_inicio = datetime.now()                        # Coge hora de inicio
+    hora_inicio = hora_inicio.strftime("%H:%M:%S")
+    input('hola')
+    hora_final = datetime.now()                         # Coge hora de finalización
+    hora_final = hora_final.strftime("%H:%M:%S")
+    cardgame = {"cardgame_id": 1, "players": 3, "start_hour": hora_inicio, "rounds":0, "final_hour": hora_final}    #Guarda todo
+    print(cardgame)
+
